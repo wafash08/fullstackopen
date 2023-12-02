713fbd5870f235af79939c960470fe8dcfeeab3c
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-02 21:40:48 +0700

    exercise part1 unicafe step-2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const total = good + neutral + bad;
+  const averageFeedback = (good - bad) / total;
+  const positiveFeedback = (good / total) * 100;
+  return (
+    <div>
+      <div>
+        <h1>give feedback</h1>
+        <div>
+          <button onClick={() => setGood(good + 1)}>good</button>
+          <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+          <button onClick={() => setBad(bad + 1)}>bad</button>
+        </div>
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {isNaN(averageFeedback) ? 0 : averageFeedback}</p>
+        <p>positive {isNaN(positiveFeedback) ? 0 : positiveFeedback}%</p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
