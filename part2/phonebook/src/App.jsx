09e23f748796908d7a68295eb5911dbac833c7b3
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-03 20:44:52 +0700

    exercise part2 phonebook step1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleAddPerson = e => {
+    e.preventDefault();
+    setPersons([
+      ...persons,
+      {
+        name: newName,
+      },
+    ]);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name:{" "}
+          <input value={newName} onChange={e => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => {
+        return <p key={person.name}>{person.name}</p>;
+      })}
+    </div>
+  );
+}
+
+export default App;
