7afc3dbd1a7310add750c3cbe14863e134fc3ec9
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-04 00:26:35 +0700

    exercise part2 phonebook step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/filter";
 import AddPersonForm from "./components/add-person-form";
 import Persons from "./components/persons";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/persons")
+      .then(response => setPersons(response.data));
+  }, []);
 
   const handleAddPerson = e => {
     e.preventDefault();
