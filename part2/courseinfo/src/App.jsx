d7e61ad98ce5f4df6a4676372cc391dfa5035b90
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-03 15:43:04 +0700

    exercise part2 courseinfo step6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/prop-types */
+function Header({ name }) {
+  return <h1>{name}</h1>;
+}
+
+function Part({ part }) {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+}
+
+function Content({ parts }) {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+}
+
+function Course({ course }) {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+}
+
+function App() {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+}
+
+export default App;
