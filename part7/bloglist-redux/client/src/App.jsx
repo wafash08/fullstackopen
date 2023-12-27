3d73ab13cd85844ff886bbf364bc0205cafe6f27
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-27 23:54:44 +0700

    7.17 redux - navigation

--- part7/bloglist-redux/client/src/App.jsx
+++ part7/bloglist-redux/client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Notification from './components/notification';
 import LoginForm from './components/login-form';
@@ -9,7 +9,28 @@ import { LS_BLOGLIST_USER, logout } from './services/auth';
 import { setNotification } from './reducers/notificationReducer';
 import { createBlog, initializeBlog } from './reducers/blogReducer';
 import { clearUser, loginOf, setUser } from './reducers/userReducer';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+
+function Navigation({ user, onLogout }) {
+	return (
+		<nav>
+			<ul style={{ listStyle: 'none', display: 'flex', gap: '0.5rem' }}>
+				<li>
+					<Link to={'/'}>blogs</Link>
+				</li>
+				<li>
+					<Link to={'/users'}>users</Link>
+				</li>
+				<li style={{ marginLeft: 'auto' }}>
+					<span>{user.name} logged in</span>
+					<button type='button' onClick={onLogout} data-test='logout_button'>
+						logout
+					</button>
+				</li>
+			</ul>
+		</nav>
+	);
+}
 
 export default function App() {
 	const user = useSelector((state) => state.user);
@@ -78,22 +99,15 @@ export default function App() {
 
 	return (
 		<div>
-			{user === null ? <h2>Log in to application</h2> : <h2>blogs</h2>}
 			<Notification />
 			{user === null ? (
-				<LoginForm onLogin={handleLogin} />
+				<>
+					<h2>Log in to application</h2>
+					<LoginForm onLogin={handleLogin} />
+				</>
 			) : (
 				<>
-					<p>
-						<span>{user.name} logged in</span>
-						<button
-							type='button'
-							onClick={handleLogout}
-							data-test='logout_button'
-						>
-							logout
-						</button>
-					</p>
+					<Navigation user={user} onLogout={handleLogout} />
 					<Togglable buttonLable={'create new blog'} ref={newBlogFormRef}>
 						<CreateNewBlogForm onCreateNewBlog={createNewBlog} />
 					</Togglable>
